b07ccab6e7c4c63edda702702a4938c0e9c586ed
michaelmedina10 <michael.cunha97@hotmail.com>
2023-09-08 08:52:17 -0300

    adicionando json para ser salvo apos o post

--- src/router/notify.py
+++ src/router/notify.py
@@ -8,7 +8,10 @@ repo_router = APIRouter(tags=['repository_notifier'])
 
 @repo_router.post('/notify')
 async def get_notification_repo(body: dict):
-    print(body)
+
+    with open('retorno_post.json', 'w') as file:
+        json.dump(body, file, ensure_ascii=True, indent=2)
+
     if body is not None:
         return JSONResponse(content=body, status_code=status.HTTP_200_OK)
     return JSONResponse(content='No Content', status_code=status.HTTP_200_OK)
